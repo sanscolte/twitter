e25ae11b1a31c27c3faa35ab6cc8d0e9807809be
Timofey <timofeykropotov@gmail.com>
2024-05-05 22:05:59 +0300

    greenlet_spawn error

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
+import asyncio
 from typing import AsyncGenerator
 
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-from src.api.models import Base
+from src.api.models import Base, User, UserFollower, Tweet, TweetLike
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
@@ -25,3 +28,63 @@ async def drop_db_and_tables():
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
+
+
+if __name__ == '__main__':
+    async def create_db():
+        async with async_session() as conn:
+            user1 = User(api_key='test1', name='Tony', tweets=[], followers=[], liked_tweets=[])
+            user2 = User(api_key='test2', name='Mike', tweets=[], followers=[], liked_tweets=[])
+            user3 = User(api_key='test3', name='John', tweets=[], followers=[], liked_tweets=[])
+            conn.add_all([user1, user2, user3])
+
+            await conn.commit()
+            await conn.refresh(user1)
+            await conn.refresh(user2)
+
+            user_follow1 = UserFollower(follower_id=1, following_id=2)
+            user_follow2 = UserFollower(follower_id=2, following_id=1)
+            user_follow3 = UserFollower(follower_id=2, following_id=3)
+            conn.add_all([user_follow1, user_follow2, user_follow3])
+
+            await conn.commit()
+            await conn.refresh(user_follow1)
+            await conn.refresh(user_follow2)
+
+            tweet1 = Tweet(content='First tweet', author_id=1)
+            tweet2 = Tweet(content='Second tweet', author_id=2)
+            tweet3 = Tweet(content='Third tweet', author_id=2)
+            conn.add_all([tweet1, tweet2, tweet3])
+
+            await conn.commit()
+            await conn.refresh(tweet1)
+            await conn.refresh(tweet2)
+
+            like1 = TweetLike(tweet_id=1, user_id=1)
+            like2 = TweetLike(tweet_id=1, user_id=2)
+            like3 = TweetLike(tweet_id=2, user_id=1)
+            like4 = TweetLike(tweet_id=2, user_id=2)
+            conn.add_all([like1, like2, like3, like4])
+
+            await conn.commit()
+
+
+    async def select_user():
+        async with async_session() as conn:
+            result = await conn.execute(
+                select(User)
+                .options(selectinload(User.tweets))
+                .options(selectinload(User.liked_tweets))
+                .options(selectinload(User.followers))
+                .where(User.api_key == 'test1')
+            )
+
+            user = result.scalar_one()
+
+            print(user)
+
+
+    asyncio.run(drop_db_and_tables())
+    asyncio.run(create_db_and_tables())
+    asyncio.run(create_db())
+    asyncio.run(select_user())
--- src/api/models.py
+++ src/api/models.py
@@ -1,22 +1,82 @@
 from typing import List
 
-from sqlalchemy import String
-from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy import ForeignKey, Column, Integer, UniqueConstraint
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship, backref
 
 
-class Base(DeclarativeBase):
-    id: Mapped[int] = mapped_column(primary_key=True)
+class Base(AsyncAttrs, DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
 
 class Tweet(Base):
     __tablename__ = 'tweet'
 
-    data: Mapped[str]
-    media_ids: Mapped[List[int] | None] = mapped_column(ARRAY(String), nullable=True)
+    content: Mapped[str]
+    attachments: Mapped[List["Media"]] = []
+    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    author: Mapped["User"] = relationship(back_populates='tweets', lazy="selectin")
+    likes: Mapped["User"] = relationship(
+        secondary="tweet_like",
+        back_populates='liked_tweets',
+        # cascade='all, delete',
+        lazy="selectin",
+    )
 
 
 class Media(Base):
     __tablename__ = 'media'
 
-    file: Mapped[str]
+    file_path: Mapped[str]
+    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'))
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    api_key: Mapped[str]
+    name: Mapped[str]
+    tweets: Mapped[List["Tweet"]] = relationship(back_populates="author", lazy="selectin")
+
+    followers: Mapped[List["UserFollower"]] = relationship(
+        "User",
+        secondary="user_follower",
+        primaryjoin="User.id==user_follower.c.follower_id",
+        secondaryjoin="User.id==user_follower.c.following_id",
+        backref=backref("following", lazy="selectin"),
+        # cascade='all, delete',
+        lazy='selectin',
+    )
+
+    liked_tweets: Mapped[List["TweetLike"]] = relationship(
+        "Tweet",
+        secondary="tweet_like",
+        back_populates="likes",
+        # cascade="all, delete",
+        lazy="selectin",
+    )
+
+    def __repr__(self) -> str:
+        return f"<User {self.name}> | {self.api_key} | Tweets {self.tweets} | Followers {self.followers} | Likes {self.liked_tweets}"
+
+
+class UserFollower(Base):
+    __tablename__ = 'user_follower'
+
+    follower_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    following_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+    __table_args__ = (
+        UniqueConstraint('follower_id', 'following_id', name='uq_follower_id_following_id'),
+    )
+
+
+class TweetLike(Base):
+    __tablename__ = 'tweet_like'
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key=True)
+    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'), primary_key=True)
+
+    __table_args__ = (
+        UniqueConstraint('tweet_id', 'user_id', name='uq_user_id_tweet_id'),
+    )
