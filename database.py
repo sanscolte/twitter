a11ca9e3d66577daacf0b3e9deca3f0f4aac18cc
Timofey <timofeykropotov@gmail.com>
2024-05-07 05:20:49 +0300

    fucking MissingGreenlet again

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ import asyncio
 from typing import AsyncGenerator
 
 from sqlalchemy import select
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import selectinload, joinedload
 from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-from src.api.models import Base, User, UserFollower, Tweet, TweetLike
+from src.api.models import Base, User, Follower, Tweet, TweetLike
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
@@ -33,38 +33,39 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 if __name__ == '__main__':
     async def create_db():
         async with async_session() as conn:
-            user1 = User(api_key='test1', name='Tony', tweets=[], followers=[], liked_tweets=[])
-            user2 = User(api_key='test2', name='Mike', tweets=[], followers=[], liked_tweets=[])
-            user3 = User(api_key='test3', name='John', tweets=[], followers=[], liked_tweets=[])
+            user1 = User(api_key='test', name='Tony')
+            user2 = User(api_key='test2', name='Mike')
+            user3 = User(api_key='test3', name='John')
             conn.add_all([user1, user2, user3])
 
             await conn.commit()
             await conn.refresh(user1)
             await conn.refresh(user2)
+            await conn.refresh(user3)
 
-            user_follow1 = UserFollower(follower_id=1, following_id=2)
-            user_follow2 = UserFollower(follower_id=2, following_id=1)
-            user_follow3 = UserFollower(follower_id=2, following_id=3)
+            user_follow1 = Follower(follower_id=1, following_id=2)
+            user_follow2 = Follower(follower_id=2, following_id=1)
+            user_follow3 = Follower(follower_id=3, following_id=1)
             conn.add_all([user_follow1, user_follow2, user_follow3])
 
-            await conn.commit()
-            await conn.refresh(user_follow1)
-            await conn.refresh(user_follow2)
-
-            tweet1 = Tweet(content='First tweet', author_id=1)
-            tweet2 = Tweet(content='Second tweet', author_id=2)
-            tweet3 = Tweet(content='Third tweet', author_id=2)
-            conn.add_all([tweet1, tweet2, tweet3])
-
-            await conn.commit()
-            await conn.refresh(tweet1)
-            await conn.refresh(tweet2)
-
-            like1 = TweetLike(tweet_id=1, user_id=1)
-            like2 = TweetLike(tweet_id=1, user_id=2)
-            like3 = TweetLike(tweet_id=2, user_id=1)
-            like4 = TweetLike(tweet_id=2, user_id=2)
-            conn.add_all([like1, like2, like3, like4])
+            # await conn.commit()
+            # await conn.refresh(user_follow1)
+            # await conn.refresh(user_follow2)
+            #
+            # tweet1 = Tweet(content='First tweet', author_id=1)
+            # tweet2 = Tweet(content='Second tweet', author_id=2)
+            # tweet3 = Tweet(content='Third tweet', author_id=2)
+            # conn.add_all([tweet1, tweet2, tweet3])
+            #
+            # await conn.commit()
+            # await conn.refresh(tweet1)
+            # await conn.refresh(tweet2)
+            #
+            # like1 = TweetLike(tweet_id=1, user_id=1)
+            # like2 = TweetLike(tweet_id=1, user_id=2)
+            # like3 = TweetLike(tweet_id=2, user_id=1)
+            # like4 = TweetLike(tweet_id=2, user_id=2)
+            # conn.add_all([like1, like2, like3, like4])
 
             await conn.commit()
 
@@ -76,18 +77,19 @@ if __name__ == '__main__':
                 .options(selectinload(User.tweets))
                 .options(selectinload(User.liked_tweets))
                 .options(selectinload(User.followers))
-                .where(User.api_key == 'test1')
+                .where(User.api_key == 'test')
             )
 
-            # P.S. любые комбинации options, любые виды подгрузки, даже без options
-            # ИМЕННО с условием НЕ работают и вызывают MissingGreenlet
+            result = result.unique()
+            user = result.scalar_one_or_none()
 
-            user = result.scalar_one()
+            # нет, joinedload не помогает
+            # нет, conn.scalars не помогает
 
-            print(user)
+            print(user.to_json())
 
 
-    asyncio.run(drop_db_and_tables())
-    asyncio.run(create_db_and_tables())
-    asyncio.run(create_db())
+    # asyncio.run(drop_db_and_tables())
+    # asyncio.run(create_db_and_tables())
+    # asyncio.run(create_db())
     asyncio.run(select_user())
--- src/api/models.py
+++ src/api/models.py
@@ -1,34 +1,91 @@
 from typing import List
 
-from sqlalchemy import ForeignKey, Column, Integer, UniqueConstraint
+from sqlalchemy import ForeignKey, UniqueConstraint, LargeBinary
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship, backref
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
 
+class Media(Base):
+    __tablename__ = 'media'
+
+    file_body: Mapped[bytes] = mapped_column(LargeBinary)
+    file_name: Mapped[str]
+    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'), nullable=False)
+    tweet: Mapped['Tweet'] = relationship(lazy='joined', back_populates='attachments')
+
+
+class TweetLike(Base):
+    __tablename__ = 'tweet_like'
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'), nullable=False)
+
+    user: Mapped["User"] = relationship(
+        lazy='joined',
+        back_populates='liked_tweets',
+        foreign_keys=[user_id]
+    )
+    tweet: Mapped["Tweet"] = relationship(
+        lazy='joined',
+        back_populates='likes',
+        foreign_keys=[tweet_id]
+    )
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'tweet_id', name='uq_user_id_tweet_id'),
+    )
+
+
 class Tweet(Base):
     __tablename__ = 'tweet'
 
     content: Mapped[str]
-    attachments: Mapped[List["Media"]] = []
+    attachments: Mapped[List["Media"]] = relationship(
+        lazy='joined',
+        back_populates='tweet',
+        cascade='all, delete-orphan',
+    )
+
     author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    author: Mapped["User"] = relationship(back_populates='tweets', lazy="selectin")
-    likes: Mapped["User"] = relationship(
-        secondary="tweet_like",
-        back_populates='liked_tweets',
-        # cascade='all, delete',
-        lazy="selectin",
+    author: Mapped["User"] = relationship(lazy='joined', back_populates='tweets')
+
+    likes: Mapped[List["TweetLike"]] = relationship(
+        lazy='joined',
+        back_populates='tweet',
+        cascade='all, delete-orphan',
     )
 
 
-class Media(Base):
-    __tablename__ = 'media'
+class Follower(Base):
+    __tablename__ = 'follower'
 
-    file_path: Mapped[str]
-    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'))
+    follower_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    following_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+    follower: Mapped['User'] = relationship(
+        lazy='joined',
+        back_populates='followers',
+        foreign_keys=[follower_id],
+    )
+    following: Mapped['User'] = relationship(
+        lazy='joined',
+        back_populates='following',
+        foreign_keys=[following_id],
+    )
+
+    def to_json(self):
+        return {
+            'id': self.following.id,
+            'name': self.following.name,
+        }
+
+    __table_args__ = (
+        UniqueConstraint('follower_id', 'following_id', name='uq_follower_id_following_id'),
+    )
 
 
 class User(Base):
@@ -36,47 +93,35 @@ class User(Base):
 
     api_key: Mapped[str]
     name: Mapped[str]
-    tweets: Mapped[List["Tweet"]] = relationship(back_populates="author", lazy="selectin")
+    tweets: Mapped[List["Tweet"]] = relationship(lazy='joined', back_populates="author")
 
-    followers: Mapped[List["UserFollower"]] = relationship(
-        "User",
-        secondary="user_follower",
-        primaryjoin="User.id==user_follower.c.following_id",
-        secondaryjoin="User.id==user_follower.c.follower_id",
-        backref=backref("following", lazy="selectin"),
-        # cascade='all, delete',
-        lazy='selectin',
+    followers: Mapped[List["Follower"]] = relationship(
+        lazy='joined',
+        back_populates='follower',
+        cascade='all, delete-orphan',
+        foreign_keys=[Follower.follower_id],
+    )
+    following: Mapped[List["Follower"]] = relationship(
+        lazy='joined',
+        back_populates='following',
+        cascade='all, delete-orphan',
+        foreign_keys=[Follower.following_id],
     )
 
     liked_tweets: Mapped[List["TweetLike"]] = relationship(
-        "Tweet",
-        secondary="tweet_like",
-        back_populates="likes",
-        # cascade="all, delete",
-        lazy="selectin",
+        lazy='joined',
+        back_populates='user',
+        cascade='all, delete-orphan',
     )
 
-    def __repr__(self) -> str:
-        return f"<User {self.name}> | {self.api_key} | Tweets {self.tweets} | Followers {self.followers} | Likes {self.liked_tweets}"
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'followers': [f.to_json() for f in self.followers],
+            'following': [f.to_json() for f in self.following],
+        }
 
-
-class UserFollower(Base):
-    __tablename__ = 'user_follower'
-
-    follower_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    following_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-
-    __table_args__ = (
-        UniqueConstraint('follower_id', 'following_id', name='uq_follower_id_following_id'),
-    )
-
-
-class TweetLike(Base):
-    __tablename__ = 'tweet_like'
-
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key=True)
-    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'), primary_key=True)
-
-    __table_args__ = (
-        UniqueConstraint('tweet_id', 'user_id', name='uq_user_id_tweet_id'),
-    )
+    # def __repr__(self) -> str:
+    #     return f"<User {self.name}> | {self.api_key} | Tweets {self.tweets} |
+    #     Followers {self.followers} | Likes {self.liked_tweets}"
