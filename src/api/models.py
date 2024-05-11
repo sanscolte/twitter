26b42bbc299794d5a045895bc24d3fb7a9e4136c
Timofey <timofeykropotov@gmail.com>
2024-05-11 21:30:17 +0300

    add models Base, Media, TweetLike, Tweet, Follower and User

--- src/api/models.py
+++ src/api/models.py
@@ -1,129 +1,149 @@
 from typing import List
 
-from sqlalchemy import ForeignKey, UniqueConstraint, LargeBinary
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    """ Базовая таблица с id """
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        unique=True,
+        autoincrement=True,
+    )
 
 
 class Media(Base):
+    """ Таблица для хранения имени и типа файла """
     __tablename__ = 'media'
 
-    file_body: Mapped[bytes] = mapped_column(LargeBinary)
-    file_name: Mapped[str]
-    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'), nullable=False)
-    tweet: Mapped['Tweet'] = relationship(lazy='joined', back_populates='attachments')
+    filename: Mapped[str]
+    content_type: Mapped[str]
+
+    tweet_id: Mapped[int] = mapped_column(
+        ForeignKey('tweet.id', ondelete='CASCADE'),
+        nullable=True,
+    )
+    tweet: Mapped['Tweet'] = relationship(
+        lazy='joined',
+        back_populates='attachments',
+    )
 
 
 class TweetLike(Base):
+    """ Таблица для хранения лайка на твит """
     __tablename__ = 'tweet_like'
 
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    tweet_id: Mapped[int] = mapped_column(ForeignKey('tweet.id'), nullable=False)
+    user_api_key: Mapped[str] = mapped_column(
+        ForeignKey('user.api_key', ondelete='CASCADE'),
+        nullable=False,
+    )
+    tweet_id: Mapped[int] = mapped_column(
+        ForeignKey('tweet.id', ondelete='CASCADE'),
+        nullable=False,
+    )
 
     user: Mapped["User"] = relationship(
         lazy='joined',
         back_populates='liked_tweets',
-        foreign_keys=[user_id]
+        foreign_keys=[user_api_key],
     )
     tweet: Mapped["Tweet"] = relationship(
         lazy='joined',
         back_populates='likes',
-        foreign_keys=[tweet_id]
+        foreign_keys=[tweet_id],
     )
 
     __table_args__ = (
-        UniqueConstraint('user_id', 'tweet_id', name='uq_user_id_tweet_id'),
+        UniqueConstraint(
+            'user_api_key',
+            'tweet_id',
+            name='uq_user_api_key_tweet_id',
+        ),
     )
 
 
 class Tweet(Base):
+    """ Таблица для хранения твита """
     __tablename__ = 'tweet'
 
     content: Mapped[str]
     attachments: Mapped[List["Media"]] = relationship(
-        lazy='joined',
         back_populates='tweet',
-        cascade='all, delete-orphan',
     )
 
-    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    author: Mapped["User"] = relationship(lazy='joined', back_populates='tweets')
+    author_api_key: Mapped[str] = mapped_column(ForeignKey('user.api_key'))
+    author: Mapped["User"] = relationship(back_populates='tweets')
 
     likes: Mapped[List["TweetLike"]] = relationship(
-        lazy='joined',
         back_populates='tweet',
-        cascade='all, delete-orphan',
     )
 
 
 class Follower(Base):
+    """ Таблица для хранения подписчиков, подписок """
     __tablename__ = 'follower'
 
-    follower_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    following_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    follower_api_key: Mapped[str] = mapped_column(
+        ForeignKey('user.api_key'),
+        nullable=False,
+    )
+    following_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'),
+        nullable=False,
+    )
 
     follower: Mapped['User'] = relationship(
         lazy='joined',
         back_populates='followers',
-        foreign_keys=[follower_id],
+        foreign_keys=[follower_api_key],
+        overlaps="following",
     )
     following: Mapped['User'] = relationship(
         lazy='joined',
         back_populates='following',
         foreign_keys=[following_id],
+        overlaps="following",
     )
 
     __table_args__ = (
-        UniqueConstraint('follower_id', 'following_id', name='uq_follower_id_following_id'),
+        UniqueConstraint(
+            'follower_api_key',
+            'following_id',
+            name='uq_follower_api_key_following_id',
+        ),
     )
-
-    def to_json_followers(self):
-        return {
-            'id': self.follower.id,
-            'name': self.follower.name,
-        }
-
-    def to_json_following(self):
-        return {
-            'id': self.following.id,
-            'name': self.following.name,
-        }
 
 
 class User(Base):
+    """ Таблица для хранения пользователя """
     __tablename__ = 'user'
 
-    api_key: Mapped[str]
+    api_key: Mapped[str] = mapped_column(primary_key=True, unique=True)
     name: Mapped[str]
-    tweets: Mapped[List["Tweet"]] = relationship(lazy='joined', back_populates="author")
+    tweets: Mapped[List["Tweet"]] = relationship(
+        lazy='joined',
+        back_populates="author",
+        cascade='all, delete-orphan',
+    )
 
     followers: Mapped[List["Follower"]] = relationship(
         lazy='joined',
         back_populates='follower',
         cascade='all, delete-orphan',
-        foreign_keys=[Follower.follower_id],
+        foreign_keys=[Follower.following_id],
+        overlaps="following",
     )
     following: Mapped[List["Follower"]] = relationship(
         lazy='joined',
         back_populates='following',
         cascade='all, delete-orphan',
-        foreign_keys=[Follower.following_id],
+        foreign_keys=[Follower.follower_api_key],
+        overlaps="following",
     )
-
     liked_tweets: Mapped[List["TweetLike"]] = relationship(
         lazy='joined',
         back_populates='user',
         cascade='all, delete-orphan',
     )
-
-    def to_json(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'followers': [f.to_json() for f in self.followers],
-            'following': [f.to_json() for f in self.following],
-        }
