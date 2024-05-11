5de64ff6a47e647d0f0eba884021daca9df030ed
Timofey <timofeykropotov@gmail.com>
2024-05-11 21:32:53 +0300

    add common alembic migration

--- alembic/env.py
+++ alembic/env.py
@@ -18,9 +18,10 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from database import Base, DATABASE_URL
+from src.api.models import Base
+from database import DATABASE_URL
 target_metadata = Base.metadata
- 
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
