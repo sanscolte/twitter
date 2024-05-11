7a42e865f74f952fdfd7665cfbd6e0d4b64f5262
Timofey <timofeykropotov@gmail.com>
2024-05-11 21:38:32 +0300

    add create_test_db method

--- database.py
+++ database.py
@@ -1,17 +1,24 @@
 import asyncio
 from typing import AsyncGenerator
 
-from sqlalchemy import select
-from sqlalchemy.orm import selectinload, joinedload
 from sqlalchemy.pool import NullPool
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker,
+    AsyncEngine,
+)
 
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-from src.api.models import Base, User, Follower, Tweet, TweetLike
+from src.api.models import (
+    Base,
+    User,
+    Tweet,
+)
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
+engine: AsyncEngine = create_async_engine(DATABASE_URL, poolclass=NullPool)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
@@ -30,66 +37,25 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
         yield session
 
 
+async def create_test_db():
+    async with async_session() as session:
+        user1: User = User(api_key='test', name='Tony')
+        user2: User = User(api_key='test2', name='Mike')
+        user3: User = User(api_key='test3', name='John')
+        session.add_all([user1, user2, user3])
+
+        await session.commit()
+        await session.refresh(user1)
+        await session.refresh(user2)
+        await session.refresh(user3)
+
+        tweet1: Tweet = Tweet(content='First tweet', author_api_key='test')
+        tweet2: Tweet = Tweet(content='Second tweet', author_api_key='test2')
+        tweet3: Tweet = Tweet(content='Third tweet', author_api_key='test3')
+        session.add_all([tweet1, tweet2, tweet3])
+
+        await session.commit()
+
+
 if __name__ == '__main__':
-    async def create_db():
-        async with async_session() as conn:
-            user1 = User(api_key='test', name='Tony')
-            user2 = User(api_key='test2', name='Mike')
-            user3 = User(api_key='test3', name='John')
-            conn.add_all([user1, user2, user3])
-
-            await conn.commit()
-            await conn.refresh(user1)
-            await conn.refresh(user2)
-            await conn.refresh(user3)
-
-            user_follow1 = Follower(follower_id=1, following_id=2)
-            user_follow2 = Follower(follower_id=2, following_id=1)
-            user_follow3 = Follower(follower_id=3, following_id=1)
-            conn.add_all([user_follow1, user_follow2, user_follow3])
-
-            # await conn.commit()
-            # await conn.refresh(user_follow1)
-            # await conn.refresh(user_follow2)
-            #
-            # tweet1 = Tweet(content='First tweet', author_id=1)
-            # tweet2 = Tweet(content='Second tweet', author_id=2)
-            # tweet3 = Tweet(content='Third tweet', author_id=2)
-            # conn.add_all([tweet1, tweet2, tweet3])
-            #
-            # await conn.commit()
-            # await conn.refresh(tweet1)
-            # await conn.refresh(tweet2)
-            #
-            # like1 = TweetLike(tweet_id=1, user_id=1)
-            # like2 = TweetLike(tweet_id=1, user_id=2)
-            # like3 = TweetLike(tweet_id=2, user_id=1)
-            # like4 = TweetLike(tweet_id=2, user_id=2)
-            # conn.add_all([like1, like2, like3, like4])
-
-            await conn.commit()
-
-
-    async def select_user():
-        async with async_session() as conn:
-            result = await conn.execute(
-                select(User)
-                .options(selectinload(User.tweets))
-                .options(selectinload(User.liked_tweets))
-                .options(selectinload(User.followers))
-                .where(User.api_key == 'test')
-            )
-
-            result = result.unique()
-            user = result.scalar_one_or_none()
-
-            # нет, joinedload не помогает
-            # нет, conn.scalars не помогает
-
-            print(user.to_json())
-
-
-    asyncio.run(drop_db_and_tables())
-    asyncio.run(create_db_and_tables())
-    asyncio.run(create_db())
-    asyncio.run(select_user())
+    asyncio.run(create_test_db())
