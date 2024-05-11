15e58e3dbb0bf22303b07aaf49a864bd327cec37
Timofey <timofeykropotov@gmail.com>
2024-05-11 21:42:38 +0300

    add api-key middleware and mount static

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request, Response, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
+from database import async_session
+from src.api.models import User
 from src.api.router import router
+from src.api.service import get_user_by_api_key
 
-app = FastAPI(
-    title="Twitter API",
-)
-
+app: FastAPI = FastAPI(title="Twitter API")
 app.include_router(router)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+templates: Jinja2Templates = Jinja2Templates(directory="templates")
+
+
+@app.middleware("http")
+async def check_user_api_key_middleware(request: Request, call_next):
+    """
+    Middleware для проверки наличия api-key в заголовках запроса и базе данных
+    :param request: Запрос
+    :param call_next: Передача запроса следующему обработчику, middleware
+    :return: Ответ
+    """
+    api_key: str = request.headers.get("api-key")
+
+    if api_key is not None:
+        async with async_session() as session:
+            user: User = await get_user_by_api_key(session, api_key)
+            if user is None:
+                raise HTTPException(status_code=401, detail="Invalid API Key")
+
+    response: Response = await call_next(request)
+    return response
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    """
+    Эндпоинт для отдачи статики
+    :param request: Запрос
+    :return: Шаблон
+    """
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
