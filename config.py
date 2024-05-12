370ba03fa85950baf28b313657d2b61ceb998b69
Timofey <timofeykropotov@gmail.com>
2024-05-12 06:41:42 +0300

    add test db envs

--- config.py
+++ config.py
@@ -10,4 +10,10 @@ DB_NAME: str | None = os.environ.get("DB_NAME")
 DB_USER: str | None = os.environ.get("DB_USER")
 DB_PASS: str | None = os.environ.get("DB_PASS")
 
+TEST_DB_HOST: str | None = os.environ.get("TEST_DB_HOST")
+TEST_DB_PORT: str | None = os.environ.get("TEST_DB_PORT")
+TEST_DB_NAME: str | None = os.environ.get("TEST_DB_NAME")
+TEST_DB_USER: str | None = os.environ.get("TEST_DB_USER")
+TEST_DB_PASS: str | None = os.environ.get("TEST_DB_PASS")
+
 FILE_DIR: str = 'static/images'
